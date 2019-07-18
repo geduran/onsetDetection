dede8df7b57cd50510ec0d658d6dbf066566e861
GABRIEL DURAN <geduran@uc.cl>
2019-07-18 11:06:52 +0800

    More intermediate messages in feature extraction between songs

--- Code/rnn_bass_DG.py
+++ Code/rnn_bass_DG.py
@@ -9,7 +9,6 @@ def get_labels_samples(dir):
     files = glob.glob(dir + '*.mid')
 
     all_labels = np.array([])
-    # all_cqt = np.array([])
     all_mel1 = np.array([])
     all_mel2 = np.array([])
     all_mel3 = np.array([])
@@ -18,10 +17,6 @@ def get_labels_samples(dir):
         curr_midi = MidiData(file)
         curr_audio = AudioData(file[:-4]+'.wav', win_len=4096, hop_len=512,
                                HPSS=False, only_bass=False)
-
-    #    curr_cqt = curr_audio.features.bass_CQT
-    #    curr_mel = curr_audio.features.bass_mel_spectrogram
-
 
 ###########################################################
         # curr_cqt = curr_audio.features.bass_CQT
@@ -45,39 +40,25 @@ def get_labels_samples(dir):
         curr_hop = curr_audio.hop_len
         curr_sr = curr_audio.sr
 
-    #    print('len audio: {}\nhoplen: {}\ncantidad muestras {}'.format(len(curr_audio.audio),
-    #            curr_audio.hop_len, curr_audio.features.chroma.shape[1]))
-    #    print('gt_bass tiene size: ' + str(len(curr_midi.gt_bass)))
-    #    print('curr_labels: {}'.format(curr_labels.shape))
-
         for time in curr_midi.gt_bass:
             index = int(time*curr_sr/curr_hop)
             if index > len(curr_labels):
                 break
             curr_labels[index] = 1
 
-        if all_labels.any() and all_mel1.any():
-           # print('all_labels {}, curr_labels {}'.format(all_labels.shape, curr_labels.shape))
+        if (all_labels.any() and all_mel1.any() and all_mel2.any() and
+            all_mel3.any()):
+            print('allmel1.shape {}, allmel2.shape {}, allmel3.shape {}'.format(allmel1.shape,allmel2.shape,allmel3.shape))
+            print('curr_mel1.shape {}, curr_mel2.shape {}, curr_mel3.shape {}'.format(curr_mel1.shape,curr_mel2.shape,curr_mel3.shape))
             all_labels = np.concatenate((all_labels, curr_labels), axis=0)
-            #print('all_samples {}, curr_samples {}'.format(all_samples.shape, curr_samples.T.shape))
-            # all_cqt = np.concatenate((all_cqt, curr_cqt.T), axis=0)
-            all_mel1 = np.concatenate((all_mel1, curr_mel1.T), axis=0)
-            all_mel2 = np.concatenate((all_mel2, curr_mel2.T), axis=0)
-            all_mel3 = np.concatenate((all_mel3, curr_mel3.T), axis=0)
-
+            all_mel1 = np.concatenate((all_mel1, curr_mel1), axis=0)
+            all_mel2 = np.concatenate((all_mel2, curr_mel2), axis=0)
+            all_mel3 = np.concatenate((all_mel3, curr_mel3), axis=0)
         else:
             all_labels = curr_labels
-            # all_cqt = curr_cqt.T
-            all_mel1 = curr_mel1.T
-            all_mel2 = curr_mel2.T
-            all_mel3 = curr_mel3.T
-
-        #print('labels.shape: {}\nsamples.shape: {}\n'.format(all_labels.shape, all_samples.shape))
-
-    #means = np.mean(all_samples, axis=0)
-    #stds = np.std(all_samples, axis=0)
-    #all_samples = np.subtract(all_samples, means)
-    #all_samples = np.divide(all_samples, stds)
+            all_mel1 = curr_mel1
+            all_mel2 = curr_mel2
+            all_mel3 = curr_mel3
 
     return all_labels, all_mel1, all_mel2, all_mel3
 
